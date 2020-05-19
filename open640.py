9b7ac1822b5d78f8bb31439ced5fda401063e4fd
Zach Pearson <zach@zjp.codes>
2020-05-19 14:14:53 -0500

    Implement QSettings

--- open640.py
+++ open640.py
@@ -12,13 +12,35 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
-class SettingsWindow():
+class Open640:
     def __init__(self):
+        self.registerDefaultSettings()
+
+    def registerDefaultSettings(self):
+        settings.beginGroup("serial")
+        settings.setValue("port", "/dev/ttyAMA0")
+        settings.setValue("baudrate", 9600)
+        settings.setValue("bytesize", serial.SEVENBITS)
+        settings.setValue("parity", serial.PARITY_EVEN)
+        settings.setValue("stopbits", serial.STOPBITS_ONE)
+        settings.setValue("xonxoff", True)
+        settings.endGroup()
+        settings.setValue("app/autoclear", False)
+
+
+class SettingsWindow(Open640):
+    def __init__(self):
+        super(SettingsWindow, self).__init__()
+        self.title = 'Open640 - Settings'
+        self.width = 400
+        self.height = 300
+        self.setMinimumSize(QSize(100,100))
+        self.initMainWindow()
         pass
 
-class Open640(QWidget):
+class MainWindow(QWidget, Open640):
     def __init__(self):
-        super(Open640, self).__init__()
+        super(MainWindow, self).__init__()
         self.title = 'Open640'
         self.width = 800
         self.height = 600
@@ -39,6 +61,9 @@ class Open640(QWidget):
         settingsButton = QPushButton('Serial Settings', self)
         settingsButton.setToolTip('Change serial port settings.')
 
+        checkSettingsButton = QPushButton('Dump Settings', self)
+        settingsButton.setToolTip('Print the current settings to the screen.')
+
         writeButton = QPushButton('Write to File', self)
         writeButton.setToolTip('Write colected data to the disk.')
 
@@ -46,28 +71,30 @@ class Open640(QWidget):
         collectToggle = QPushButton('Start Collection', self)
 
         settingsButton.clicked.connect(lambda: self.onSettingsButtonClicked())
+        checkSettingsButton.clicked.connect(lambda: self.onCheckButtonClicked(dataArea))
         collectToggle.clicked.connect(lambda: self.onStartButtonClicked(dataArea))
         writeButton.clicked.connect(lambda: self.onWriteButtonClicked(dataArea))
         clearButton.clicked.connect(lambda: self.onClearOutputClicked(dataArea))
 
         layout.addWidget(dataArea, 0, 0, 1, 0)
         layout.addWidget(settingsButton, 1, 0)
-        layout.addWidget(clearButton, 1, 1)
-        layout.addWidget(collectToggle, 1, 2)
-        layout.addWidget(writeButton, 1, 3)
+        layout.addWidget(checkSettingsButton, 1, 1)
+        layout.addWidget(clearButton, 1, 2)
+        layout.addWidget(collectToggle, 1, 3)
+        layout.addWidget(writeButton, 1, 4)
 
         self.show()
 
     def collectData():
         self.currently_collecting = True
         try:
-            ser = serial.Serial(
-                    self.port,
-                    baudrate = self.baudrate,
-                    bytesize = self.bytesize,
-                    parity = self.parity,
-                    stopbits = self.stopbits,
-                    xonxoff = self.xonxoff)
+            ser = serial.Serial( 
+                    settings.value("serial/port"),
+                    baudrate = settings.value("serial/baudrate"),
+                    bytesize = settings.value("serial/bytesize"),
+                    parity = settings.value("serial/parity"),
+                    stopbits = settings.value("serial/stopbits"),
+                    xonxoff = settings.value("serial/xonxoff"))
             data = ser.read(1)
             output.setPlainText(output.toPlainText() + data_str.decode('ascii'))
         except serial.SerialException:
@@ -83,12 +110,12 @@ class Open640(QWidget):
             self.currently_collecting = True
             try:
                 ser = serial.Serial(
-                        self.port,
-                        baudrate = self.baudrate,
-                        bytesize = self.bytesize,
-                        parity = self.parity,
-                        stopbits = self.stopbits,
-                        xonxoff = self.xonxoff)
+                        settings.value("serial/port"),
+                        baudrate = settings.value("serial/baudrate"),
+                        bytesize = settings.value("serial/bytesize"),
+                        parity = settings.value("serial/parity"),
+                        stopbits = settings.value("serial/stopbits"),
+                        xonxoff = settings.value("serial/xonxoff"))
                 print("2")
                 sender.setText('Stop Collection')
                 # Dodgy reimplementation of do-while by copying code
@@ -144,16 +171,20 @@ class Open640(QWidget):
     def onSettingsButtonClicked(self): 
         pass
 
-    # Default Settings
-    port = "/dev/ttyAMA0"
-    baudrate = 9600
-    bytesize = serial.SEVENBITS
-    parity = serial.PARITY_EVEN
-    stopbits = serial.STOPBITS_ONE
-    xonxoff = True
-    autoclear = False
+    def onCheckButtonClicked(self, output):
+        output.setPlainText(
+                "Current Settings:" +
+                "\n\tPort: " + settings.value("serial/port") + 
+                "\n\tBaudrate: " + str(settings.value("serial/baudrate")) +
+                "\n\tBytesize: " + str(settings.value("serial/bytesize")) + " bits"
+                "\n\tParity: " + settings.value("serial/parity") +
+                "\n\tStop Bits: " + str(settings.value("serial/stopbits")) +
+                "\n\tFlow Control: " + str(settings.value("serial/xonxoff"))
+        )
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    GUI = Open640()
+    orgName = "boskoviclab.ku.edu" if sys.platform.startswith('darwin') else "BoskovicGroup"
+    settings = QSettings(orgName, "Open640")
+    MainWindow()
     sys.exit(app.exec_())

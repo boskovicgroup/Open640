49425a7d607ad9c10d392a55a1d4550e9da82811
Zach Pearson <zach@zjp.codes>
2020-05-26 18:03:34 -0500

    Settings Menu

--- open640.py
+++ open640.py
@@ -14,7 +14,9 @@ from PyQt5.QtCore import *
 
 class Open640:
     def __init__(self):
-        # Only write a new settings file if one does not exist
+        # Only write a new settings file if one does not exist. In this case,
+        # checking for a particular setting is equivalent to checking for the
+        # file itself.
         if not settings.contains("serial/xonxoff"):
             self.registerDefaultSettings()
 
@@ -29,16 +31,150 @@ class Open640:
         settings.endGroup()
         settings.setValue("app/autoclear", False)
 
+    def validateByteSize(self, bits):
+        if(bits == 5):
+            return serial.FIVEBITS
+        if(bits == 6):
+            return serial.SIXBITS
+        if(bits == 7):
+            return serial.SEVENBITS
+        if(bits == 8):
+            return serial.EIGHTBITS
+        raise ValueError("Bytesize must be in the range [5, 8]")
 
-class SettingsWindow(Open640):
+    def validateParity(self, parity):
+        parity = parity.lower()
+        if(parity == 'e' or parity == 'even'):
+            return serial.PARITY_EVEN
+        if(parity == 'o' or parity == 'odd'):
+            return serial.PARITY_ODD
+        if(parity == 'm' or parity == 'mark'):
+            return serial.PARITY_MARK
+        if(parity == 's' or parity == 'space'):
+            return serial.PARITY_SPACE
+        if(parity == 'n' or parity == 'none'):
+            return serial.PARITY_NONE
+        raise ValueError("Parity must be one of: even, odd, mark, space, or none")
+
+    def validateParityTextBox(self, parity):
+        if(parity == 'E'):
+            return "Even"
+        if(parity == 'O'):
+            return "Odd"
+        if(parity == 'M'):
+            return "Mark"
+        if(parity == 'S'):
+            return "Space"
+        if(parity == 'N'):
+            return "None"
+
+    def validateStopBits(self, bits):
+        if(bits == 1):
+            return serial.STOPBITS_ONE
+        if(bits == 1.5):
+            return serial.STOPBITS_ONE_POINT_FIVE
+        if(bits == 2):
+            return serial.STOPBITS_TWO
+        raise ValueError("Stopbits must be 1, 1.5, or 2. We don't know what 1.5 means either.")
+
+class SettingsWindow(QDialog, Open640):
     def __init__(self):
         super(SettingsWindow, self).__init__()
-        self.title = 'Open640 - Settings'
         self.width = 400
         self.height = 300
-        self.setMinimumSize(QSize(100,100))
-        self.initMainWindow()
-        pass
+        self.setMinimumSize(QSize(320,240))
+        self.initSettingsWindow()
+
+    def initSettingsWindow(self):
+        self.setWindowTitle('Open640 - Settings')
+        layout = QGridLayout(self)
+        layout.setSpacing(10)
+        self.setLayout(layout)
+
+        self.portLabel = QLabel("Port")
+        self.baudLabel = QLabel("Baudrate")
+        self.byteLabel = QLabel("Byte Size")
+        self.parityLabel = QLabel("Parity")
+        self.stopLabel = QLabel("Stop Bits")
+
+
+        self.portBox = QLineEdit(self)
+        self.portBox.setText(settings.value("serial/port"))
+        self.portBox.setToolTip("The address of the port that will be used for communication.")
+
+        self.baudrateBox = QLineEdit(self)
+        self.baudrateBox.setText(str(settings.value("serial/baudrate")))
+        self.baudrateBox.setToolTip("Communications speed; this must match the setting on the DU-640.")
+
+        self.byteBox = QLineEdit(self)
+        self.byteBox.setText(str(settings.value("serial/bytesize")))
+        self.byteBox.setToolTip("Size of incoming data, in bits. This must match the setting on the DU-640.")
+
+        self.parityBox = QLineEdit(self)
+        self.parityBox.setText(self.validateParityTextBox(str(settings.value("serial/parity"))))
+        self.parityBox.setToolTip("Parity checking type. Can be even, odd, or none. This must match the setting on the DU-640.")
+
+        self.stopBox = QLineEdit(self)
+        self.stopBox.setText(str(settings.value("serial/stopbits")))
+        self.stopBox.setToolTip("Number of stop bits. Can be 1, 1.5, or 2. This must match the setting on the DU-640.")
+
+        self.fcButton = QCheckBox(self)
+        self.fcButton.setText("Software Flow Control")
+        self.fcButton.setChecked(True if settings.value("serial/xonxoff")=='true' else False)
+        self.fcButton.setToolTip("Enable or disable flow control to control DU-640 from a computer. This must match the setting on the DU-640.")
+
+        self.autoclearButton = QCheckBox(self)
+        self.autoclearButton.setChecked(True if settings.value("app/autoclear")=='true' else False)
+        self.autoclearButton.setText("Autoclear output on save")
+
+        self.saveButton = QPushButton('Save Changes')
+
+        self.closeButton = QPushButton('Close Dialog')
+
+        self.saveButton.clicked.connect(lambda: self.onSaveButtonClicked())
+        self.closeButton.clicked.connect(lambda: self.onCloseButtonClicked())
+
+        row1 = [self.portLabel, self.baudLabel, self.byteLabel, self.parityLabel]
+        row2 = [self.portBox, self.baudrateBox, self.byteBox, self.parityBox]
+        row4 = [self.stopBox, self.fcButton, self.autoclearButton]
+        for index, row in enumerate([row1, row2]):
+            for entry in zip(row, range(0,4)):
+                layout.addWidget(entry[0], index, entry[1])
+        layout.addWidget(self.stopLabel, 2, 0)
+        for entry in zip(row4, range(0,3)):
+                layout.addWidget(entry[0], 3, entry[1])
+        layout.addWidget(self.closeButton, 4, 2)
+        layout.addWidget(self.saveButton, 4, 3)
+
+        self.show()
+
+    def onCloseButtonClicked(self):
+        self.close()
+
+    def onSaveButtonClicked(self):
+        try:
+            localBytesize = self.validateByteSize(int(self.byteBox.text()))
+            localParity = self.validateParity(self.parityBox.text())
+            localStopbits = self.validateStopBits(float(self.stopBox.text()))
+            du640_baudrates = [4800, 9600, 19200]
+            if int(self.baudrateBox.text()) not in du640_baudrates:
+                raise ValueError("Baudrate must be one of 4800, 9600, 19200")
+        except ValueError as e:
+            alert = QMessageBox()
+            alert.setText("There is something wrong with your settings, and they have not yet been committed.\n\nError: " + str(e))
+            alert.exec_()
+            return
+
+        settings.beginGroup("serial")
+        settings.setValue("port", self.portBox.text())
+        settings.setValue("baudrate", int(self.baudrateBox.text()))
+        settings.setValue("bytesize", localBytesize)
+        settings.setValue("parity", localParity)
+        settings.setValue("stopbits", localStopbits)
+        settings.setValue("xonxoff", self.fcButton.isChecked())
+        settings.endGroup()
+        settings.setValue("app/autoclear", self.autoclearButton.isChecked())
+        self.close()
 
 class MainWindow(QWidget, Open640):
     def __init__(self):
@@ -90,13 +226,13 @@ class MainWindow(QWidget, Open640):
     def collectData():
         self.currently_collecting = True
         try:
-            ser = serial.Serial( 
-                    settings.value("serial/port"),
-                    baudrate = settings.value("serial/baudrate"),
-                    bytesize = settings.value("serial/bytesize"),
-                    parity = settings.value("serial/parity"),
-                    stopbits = settings.value("serial/stopbits"),
-                    xonxoff = settings.value("serial/xonxoff"))
+            ser = serial.Serial(
+                settings.value("serial/port"),
+                baudrate = settings.value("serial/baudrate"),
+                bytesize = settings.value("serial/bytesize"),
+                parity = settings.value("serial/parity"),
+                stopbits = settings.value("serial/stopbits"),
+                xonxoff = settings.value("serial/xonxoff"))
             data = ser.read(1)
             output.setPlainText(output.toPlainText() + data_str.decode('ascii'))
         except serial.SerialException:
@@ -112,12 +248,12 @@ class MainWindow(QWidget, Open640):
             self.currently_collecting = True
             try:
                 ser = serial.Serial(
-                        settings.value("serial/port"),
-                        baudrate = settings.value("serial/baudrate"),
-                        bytesize = settings.value("serial/bytesize"),
-                        parity = settings.value("serial/parity"),
-                        stopbits = settings.value("serial/stopbits"),
-                        xonxoff = settings.value("serial/xonxoff"))
+                    settings.value("serial/port"),
+                    baudrate = settings.value("serial/baudrate"),
+                    bytesize = settings.value("serial/bytesize"),
+                    parity = settings.value("serial/parity"),
+                    stopbits = settings.value("serial/stopbits"),
+                    xonxoff = settings.value("serial/xonxoff"))
                 print("2")
                 sender.setText('Stop Collection')
                 # Dodgy reimplementation of do-while by copying code
@@ -127,23 +263,23 @@ class MainWindow(QWidget, Open640):
                 output.setPlainText(output.toPlainText() + data_str.decode('ascii'))
                 print(output.toPlainText())
                 print("Waiting: " + str(ser.inWaiting()))
-                time.sleep(0.01)  
+                time.sleep(0.01)
                 while True:
                     if(ser.inWaiting() > 0):
                         data_str = ser.read(1)
                         output.setPlainText(output.toPlainText() + data_str.decode('ascii'))
                         print(output.toPlainText())
                         print("Waiting: " + str(ser.inWaiting()))
-                    else: 
+                    else:
                         break
-                print("5")
-                self.currently_collecting = False
-                sender.setText('Start Collection') 
+                    print("5")
+                    self.currently_collecting = False
+                    sender.setText('Start Collection')
             except serial.SerialException:
                 alert = QMessageBox()
                 alert.setText('Could not open serial port. Ensure /dev/ttyAMA0 exists and is available, then try again.')
                 alert.exec_()
-        else: 
+        else:
             self.currently_collecting = False
             try:
                 if ser is not None:
@@ -159,31 +295,34 @@ class MainWindow(QWidget, Open640):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         filename, _ = QFileDialog.getSaveFileName(
-                self,
-                "QFileDialog.getSaveFileName()",
-                "",
-                "Text Files (*.txt)",
-                options=options)
+            self,
+            "QFileDialog.getSaveFileName()",
+            "",
+            "Text Files (*.txt)",
+            options=options)
         if filename:
             print("Saving to " + filename + ".txt...")
             f = open(filename + '.txt', 'w')
-            f.write(widget.toPlainText())
+            f.write(widget.toPlainText() + '\n')
             f.close()
 
-    def onSettingsButtonClicked(self): 
-        pass
+    def onSettingsButtonClicked(self):
+        dlg = SettingsWindow()
+        dlg.exec_()
 
     def onCheckButtonClicked(self, output):
         output.setPlainText(
-                "Current Settings:" +
-                "\n\tPort: " + settings.value("serial/port") + 
-                "\n\tBaudrate: " + str(settings.value("serial/baudrate")) +
-                "\n\tBytesize: " + str(settings.value("serial/bytesize")) + " bits"
-                "\n\tParity: " + settings.value("serial/parity") +
-                "\n\tStop Bits: " + str(settings.value("serial/stopbits")) +
-                "\n\tFlow Control: " + str(settings.value("serial/xonxoff"))
+            "Current Settings:" +
+            "\n\tPort: " + settings.value("serial/port") +
+            "\n\tBaudrate: " + str(settings.value("serial/baudrate")) +
+            "\n\tBytesize: " + str(settings.value("serial/bytesize")) + " bits"
+            "\n\tParity: " + settings.value("serial/parity") +
+            "\n\tStop Bits: " + str(settings.value("serial/stopbits")) +
+            "\n\tFlow Control: " + str(settings.value("serial/xonxoff")) +
             "\n\nRemember that settings are persistent."
         )
+
+    currently_collecting = False
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)

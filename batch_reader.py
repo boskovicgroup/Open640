9d260f1ff639511eaa2cdd5dd4bd272999867a73
Zach Pearson <zach@zjp.codes>
2021-01-05 15:07:37 -0600

    Start button for batch reader

--- batch_reader.py
+++ batch_reader.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton
 from PyQt5.QtCore import QSize
 
 
@@ -6,13 +6,23 @@ class BatchWindow(QWidget):
     def __init__(self):
         super(BatchWindow, self).__init__()
         self.title = "Open640 - Batch Collector"
+        self.setWindowTitle(self.title)
         self.width = 800
         self.height = 600
         self.setMinimumSize(QSize(320, 240))
         self.initLayout()
 
     def initLayout(self):
-        pass
+        layout = QGridLayout()
+        layout.setSpacing(10)
+        self.setLayout(layout)
+
+        self.startButton = QPushButton('Start Collection')
+        self.startButton.setToolTip('Begin a batch collection.')
+
+        self.startButton.clicked.connect(lambda: self.onStartButtonClicked())
+
+        layout.addWidget(self.startButton, 0, 0, 1, 0)
 
     def onStartButtonClicked(self):
         pass
